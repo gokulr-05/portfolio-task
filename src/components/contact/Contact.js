e3bb2140210831ef7ff8b9f9caf9e42a83dc95d6
gokulr-05 <gokulravivcr05@gmail.com>
2022-08-24 19:55:52 +0530

    skills

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,29 @@
+import "./contact.css";
+let Contact = function () {
+  return (
+    <div className="contact-area">
+      <div className="contact-sec">
+        <h1 className="text-center text-primary">Contact</h1>
+        <div className="contact-form-container">
+          <input placeholder="Name" type="text" className="form-control my-3" />
+          <input
+            placeholder="Email"
+            type="text"
+            className="form-control my-3"
+          />
+          <textarea
+            className="form-control"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+
+          <button className="btn btn-primary mt-3">Contact</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
